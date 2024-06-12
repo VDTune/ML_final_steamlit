73f2313ad4eb645ad30f6ee1734c198c0528659d
Nguyen Van Vi <talavyfc12345678@gmail.com>
2024-06-12 22:33:28 +0700

    push 12/06/2024

--- test/test.py
+++ test/test.py
@@ -19,17 +19,16 @@ from sklearn.metrics import (
 )
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
 
-# Thiết lập tiêu đề chính
-st.title("Ứng dụng Machine Learning")
+# Set main title
+st.title("Machine Learning Application")
 
 def display_statistics(df, target_variable, independent_variables):
-    st.write("## Thống kê của biến mục tiêu")
+    st.write("## Target Variable Statistics")
     st.write(df[target_variable].value_counts())
 
-    st.write("## Thống kê của các biến độc lập")
+    st.write("## Independent Variables Statistics")
     for col in independent_variables:
         st.write(df[col].describe())
 
@@ -40,7 +39,7 @@ def display_correlation(df):
 
     plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
-    plt.title("Biểu đồ tương quan giữa các thuộc tính với biến lớp", fontsize=15, fontweight="bold")
+    plt.title("Correlation Matrix", fontsize=15, fontweight="bold")
     st.pyplot(plt.gcf())
 
 def preprocess_data(df, target_variable, independent_variables):
@@ -64,63 +63,43 @@ def scale_data(X_train, X_test):
     X_test = scaler.transform(X_test)
     return X_train, X_test
 
-from sklearn.neighbors import NearestNeighbors
-
 def recommend(user_id, df, user_col, song_col, rating_col, n_recommendations, df_movies):
-    # Tạo ma trận tiện ích
     utility_matrix = df.pivot_table(index=user_col, columns=song_col, values=rating_col)
-    
-    # Xử lý các giá trị NaN bằng median của từng cột
     imputer = SimpleImputer(strategy='median')
     utility_matrix_imputed = pd.DataFrame(imputer.fit_transform(utility_matrix), index=utility_matrix.index, columns=utility_matrix.columns)
-    
-    # Chuẩn hóa dữ liệu
     scaler = StandardScaler()
     utility_matrix_scaled = pd.DataFrame(scaler.fit_transform(utility_matrix_imputed), index=utility_matrix_imputed.index, columns=utility_matrix_imputed.columns)
     
-    # Tạo mô hình KNN
     model_knn = NearestNeighbors(metric='cosine', algorithm='brute')
     model_knn.fit(utility_matrix_scaled)
 
     user_id = int(user_id)
     st.write(f"User ID entered: {user_id}, type: {type(user_id)}")
-    # Kiểm tra xem user_id có trong ma trận tiện ích không
     if user_id not in utility_matrix_scaled.index:
         raise ValueError("User ID not found in the dataset")
     
-    # Tìm kiếm hàng xóm gần nhất
     user_index = utility_matrix_scaled.index.get_loc(user_id)
     distances, indices = model_knn.kneighbors([utility_matrix_scaled.iloc[user_index, :]], n_neighbors=n_recommendations + 1)
     
-    # Tạo danh sách các bài hát gợi ý
     recommendations = []
     for i in range(1, len(distances.flatten())):
         song_id = utility_matrix_scaled.columns[indices.flatten()[i]]
-        # Tìm tiêu đề phim tương ứng
         title = df_movies.loc[df_movies[song_col] == song_id, 'title'].values[0]
         recommendations.append(title)
     
     return recommendations
 
-
-# Tải file lên từ thanh bên
-uploaded_file = st.sidebar.file_uploader("Chọn CSV", type="csv")
+uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     df = df.where(pd.notnull(df), None)
 
-    st.write("## Thông tin Dataset")
-    st.write(df)
-    st.write(f"Số dòng: {df.shape[0]}")
-    st.write(f"Số cột: {df.shape[1]}")
-    st.write(f"Các cột: {df.columns.tolist()}")
-
-    model_type = st.sidebar.selectbox("Chọn loại mô hình", [
+    model_type = st.sidebar.selectbox("Choose Model Type", [
         "Logistic Regression", "KNN", "Random Forest", "Decision Tree", "Linear Regression"])
 
     if model_type == "KNN":
-        k = st.sidebar.number_input("Chọn K", 1, len(df), 5)
+        k = st.sidebar.number_input("Choose K", 1, len(df), 5)
         model = KNeighborsClassifier(n_neighbors=k)
     elif model_type == "Logistic Regression":
         model = LogisticRegression()
@@ -131,142 +110,135 @@ if uploaded_file is not None:
     elif model_type == "Linear Regression":
         model = LinearRegression()
 
-    target_variable = st.sidebar.selectbox("Chọn biến mục tiêu", df.columns, index=len(df.columns) - 1)
-    independent_variables = st.sidebar.multiselect("Chọn biến độc lập", df.columns, default=list(df.columns.drop(target_variable)))
-    # Tạo giao diện người dùng để nhập dữ liệu dự đoán
-    st.write("## Dự đoán từ dữ liệu mới")
-    input_data = {}
-    for col in independent_variables:
-        valueInf = df.iloc[1][col]
-        value = st.text_input(f"Nhập giá trị cho '{col}':", valueInf)
-        input_data[col] = value
+    target_variable = st.sidebar.selectbox("Choose Target Variable", df.columns, index=len(df.columns) - 1)
+    independent_variables = st.sidebar.multiselect("Choose Independent Variables", df.columns, default=list(df.columns.drop(target_variable)))
 
-    if st.sidebar.button("Huấn luyện mô hình"):
-        if not independent_variables:
-            st.error("Vui lòng chọn ít nhất một biến độc lập.")
-        else:
-            try:
-                X_train, X_test, Y_train, Y_test = preprocess_data(df, target_variable, independent_variables)
-                X_train, X_test = scale_data(X_train, X_test)
+    # Add tabs
+    tabs = st.tabs(["Dataset Information", "Model Training", "Predictions"])
 
-                model.fit(X_train, Y_train)
-                Y_pred = model.predict(X_test)
+    with tabs[0]:
+        st.write("## Dataset Information")
+        st.write(df)
+        st.write(f"Number of Rows: {df.shape[0]}")
+        st.write(f"Number of Columns: {df.shape[1]}")
+        st.write(f"Columns: {df.columns.tolist()}")
+        display_statistics(df, target_variable, independent_variables)
+        display_correlation(df)
 
-                # Xử lý dữ liệu đầu vào cho dự đoán
-                input_df = pd.DataFrame([input_data])
-
-                # Kiểm tra dữ liệu đầu vào có thiếu giá trị không
-                if input_df.isnull().values.any():
-                    st.error("Vui lòng điền vào tất cả các trường.")
-
-                # Chuyển đổi dữ liệu đầu vào thành định dạng số nếu cần
-                le = LabelEncoder()
-                for col in input_df.columns:
-                    if input_df[col].dtype == "object":
-                        le.fit(input_df[col])
-                        input_df[col] = le.transform(input_df[col])
-
-                # Dự đoán trên dữ liệu mới
+    with tabs[1]:
+        if st.button("Train Model"):
+            if not independent_variables:
+                st.error("Please choose at least one independent variable.")
+            else:
                 try:
-                    prediction = model.predict(input_df)
-                    st.write("## Kết quả Dự đoán")
-                    st.write(f"{target_variable}: {prediction[0]}")
-                except Exception as e:
-                    st.error(f"Đã xảy ra lỗi khi dự đoán: {e}")
-                # st.subheader("Kết quả dự đoán")
-                # st.write(f"Dự đoán {target_variable}: {Y_pred[0]}")
+                    X_train, X_test, Y_train, Y_test = preprocess_data(df, target_variable, independent_variables)
+                    X_train, X_test = scale_data(X_train, X_test)
 
-                if model_type in ["Logistic Regression", "KNN", "Random Forest", "Decision Tree"]:
-                    accuracy = accuracy_score(Y_test, Y_pred)
-                    precision = precision_score(Y_test, Y_pred, average="weighted")
-                    recall = recall_score(Y_test, Y_pred, average="weighted")
-                    f1 = f1_score(Y_test, Y_pred, average="weighted")
+                    model.fit(X_train, Y_train)
+                    Y_pred = model.predict(X_test)
 
-                    st.write("## Đánh giá Mô hình")
-                    st.write(f"Độ chính xác: {accuracy:.2f}")
-                    st.write(f"Độ chính xác (Precision): {precision:.2f}")
-                    st.write(f"Khả năng hồi đáp (Recall): {recall:.2f}")
-                    st.write(f"F1 Score: {f1:.2f}")
+                    st.write("## Model Evaluation")
 
-                    st.write("## Ma trận nhầm lẫn")
-                    conf_matrix = confusion_matrix(Y_test, Y_pred)
-                    plt.figure(figsize=(8, 6))
-                    sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt="g")
-                    plt.xlabel("Nhãn dự đoán")
-                    plt.ylabel("Nhãn thực")
-                    plt.title("Ma trận nhầm lẫn")
-                    st.pyplot(plt.gcf())
+                    if model_type in ["Logistic Regression", "KNN", "Random Forest", "Decision Tree"]:
+                        accuracy = accuracy_score(Y_test, Y_pred)
+                        precision = precision_score(Y_test, Y_pred, average="weighted")
+                        recall = recall_score(Y_test, Y_pred, average="weighted")
+                        f1 = f1_score(Y_test, Y_pred, average="weighted")
 
-                    st.write("## Biểu đồ tương quan giữa các thuộc tính với biến lớp")
-                    display_correlation(df)
+                        st.write(f"Accuracy: {accuracy:.2f}")
+                        st.write(f"Precision: {precision:.2f}")
+                        st.write(f"Recall: {recall:.2f}")
+                        st.write(f"F1 Score: {f1:.2f}")
 
-                    st.write("## Biểu đồ scatter plot của các cặp biến độc lập")
-                    scatter_plot = sns.pairplot(df[independent_variables])
-                    st.pyplot(scatter_plot)
-
-                    st.write("## Biểu đồ phân phối của các biến độc lập")
-                    for col in independent_variables:
-                        plt.figure(figsize=(4, 2))
-                        sns.histplot(df[col], kde=True)
-                        plt.xlabel(col)
-                        plt.ylabel("Số lượng")
-                        plt.title(f"Phân phối của {col}")
+                        st.write("## Confusion Matrix")
+                        conf_matrix = confusion_matrix(Y_test, Y_pred)
+                        plt.figure(figsize=(8, 6))
+                        sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt="g")
+                        plt.xlabel("Predicted Label")
+                        plt.ylabel("True Label")
+                        plt.title("Confusion Matrix")
                         st.pyplot(plt.gcf())
 
-                elif model_type == "Linear Regression":
-                    mae = mean_absolute_error(Y_test, Y_pred)
-                    mse = mean_squared_error(Y_test, Y_pred)
-                    r2 = r2_score(Y_test, Y_pred)
+                    elif model_type == "Linear Regression":
+                        mae = mean_absolute_error(Y_test, Y_pred)
+                        mse = mean_squared_error(Y_test, Y_pred)
+                        r2 = r2_score(Y_test, Y_pred)
 
-                    st.write("## Đánh giá Mô hình")
-                    st.write(f"Mean Absolute Error: {mae:.2f}")
-                    st.write(f"Mean Squared Error: {mse:.2f}")
-                    st.write(f"R² Score: {r2:.2f}")
+                        st.write(f"Mean Absolute Error: {mae:.2f}")
+                        st.write(f"Mean Squared Error: {mse:.2f}")
+                        st.write(f"R² Score: {r2:.2f}")
 
-                    if len(independent_variables) == 1:
-                        plt.figure(figsize=(10, 6))
-                        plt.scatter(X_test[:, 0], Y_test, color="black", label="Data")
-                        plt.plot(X_test[:, 0], Y_pred, color="blue", linewidth=3, label="Regression line")
-                        plt.xlabel(independent_variables[0])
-                        plt.ylabel(target_variable)
-                        plt.title("Linear Regression")
-                        plt.legend()
-                        st.pyplot(plt)
-                    elif len(independent_variables) == 2:
-                        fig = plt.figure(figsize=(10, 6))
-                        ax = fig.add_subplot(111, projection="3d")
-                        ax.scatter(X_test[:, 0], X_test[:, 1], Y_test, color="black", label="Data")
-                        ax.set_xlabel(independent_variables[0])
-                        ax.set_ylabel(independent_variables[1])
-                        ax.set_zlabel(target_variable)
-                        ax.set_title("Multiple Linear Regression")
-                        ax.view_init(45, 0)
-                        ax.legend()
-                        st.pyplot(fig)
-                    else:
-                        st.write("Không thể vẽ đồ thị với hơn 2 biến độc lập.")
-            except Exception as e:
-                st.error(f"Đã xảy ra lỗi: {e}")
+                        if len(independent_variables) == 1:
+                            plt.figure(figsize=(10, 6))
+                            plt.scatter(X_test[:, 0], Y_test, color="black", label="Data")
+                            plt.plot(X_test[:, 0], Y_pred, color="blue", linewidth=3, label="Regression line")
+                            plt.xlabel(independent_variables[0])
+                            plt.ylabel(target_variable)
+                            plt.title("Linear Regression")
+                            plt.legend()
+                            st.pyplot(plt)
+                        elif len(independent_variables) == 2:
+                            fig = plt.figure(figsize=(10, 6))
+                            ax = fig.add_subplot(111, projection="3d")
+                            ax.scatter(X_test[:, 0], X_test[:, 1], Y_test, color="black", label="Data")
+                            ax.set_xlabel(independent_variables[0])
+                            ax.set_ylabel(independent_variables[1])
+                            ax.set_zlabel(target_variable)
+                            ax.set_title("Multiple Linear Regression")
+                            ax.view_init(45, 0)
+                            ax.legend()
+                            st.pyplot(fig)
+                        else:
+                            st.write("Cannot plot more than 2 independent variables.")
 
-    st.sidebar.subheader("Gợi ý theo đánh giá")
-    # item_index = st.sidebar.number_input("Chọn chỉ số mục tiêu (index)", min_value=0, max_value=len(df)-1, value=0)
-    num_recommendations = st.sidebar.slider("Số lượng gợi ý", 1, 10, 5)
+                    # Store the trained model for prediction use
+                    st.session_state['model'] = model
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
 
-    # Hiển thị danh sách các cột để người dùng chọn
+    with tabs[2]:
+        st.write("## Make Predictions")
+        input_data = {}
+        for col in independent_variables:
+            valueInf = df.iloc[1][col]
+            value = st.text_input(f"Enter value for '{col}':", valueInf)
+            input_data[col] = value
+
+        if st.button("Predict"):
+            if 'model' not in st.session_state:
+                st.error("Model is not trained yet. Please train the model first.")
+            else:
+                try:
+                    model = st.session_state['model']
+                    input_df = pd.DataFrame([input_data])
+
+                    if input_df.isnull().values.any():
+                        st.error("Please fill in all the fields.")
+
+                    le = LabelEncoder()
+                    for col in input_df.columns:
+                        if input_df[col].dtype == "object":
+                            le.fit(input_df[col])
+                            input_df[col] = le.transform(input_df[col])
+
+                    prediction = model.predict(input_df)
+                    st.write("## Prediction Result")
+                    st.write(f"{target_variable}: {prediction[0]}")
+                except Exception as e:
+                    st.error(f"An error occurred during prediction: {e}")
+
+    st.sidebar.subheader("Recommendations")
+    num_recommendations = st.sidebar.slider("Number of Recommendations", 1, 10, 5)
     user_col = st.sidebar.selectbox("Select User Column", df.columns)
     song_col = st.sidebar.selectbox("Select Song Column", df.columns)
     rating_col = st.sidebar.selectbox("Select Rating Column", df.columns)
-    # Nhập user_id từ người dùng
     user_id = st.sidebar.text_input("Enter User ID")
     
-    if st.sidebar.button("Gợi ý theo đánh giá"):
+    if st.sidebar.button("Get Recommendations"):
         try:
             movies = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/movies.csv")
             recommendations = recommend(user_id, df, user_col, song_col, rating_col, num_recommendations, movies)
-            st.write("## Kết quả gợi ý")
+            st.write("## Recommendations")
             st.write(recommendations)
         except Exception as e:
-            st.error(f"Đã xảy ra lỗi khi gợi ý: {e}")
-    st.sidebar.subheader("Gợi ý theo từ khóa tương tự")
-    
-    # if st.sidebar.button("Gợi ý theo từ khóa"):
+            st.error(f"An error occurred during recommendation: {e}")
